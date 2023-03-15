868b65d882ea5110268c09590fe27860f11fc395
Ataize Feitosa <isa.aragao@hotmail.com>
2023-03-15 10:21:59 -0300

    Primeiro Commit

---
+++ assets/scripts/dist/index.js
@@ -0,0 +1,11 @@
+// console.log("vai Corinthians");
+// type escreveProps = {
+//   name?: string;
+//   email?: string;
+//   login?: string;
+// };
+// function escreve(name: escreveProps): string {
+//   return `Seja bem-vindo(a) ${name?.name}`;
+// }
+// console.log(escreve({ name: "Ataize" }));
+import "./main.js";
